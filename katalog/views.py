b0b084dfe4f7959417f3a31ce77389fb45de9e25
rionadani <rionadani@gmail.com>
2022-09-14 21:45:15 +0700

    Menyelesaikan Tugas 1

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'list_barang': data_barang_katalog,
+        'nama': 'Riona Adani Ghaisani',
+        'npm' : '2106750995'
+    }
+    return render(request, "katalog.html", context)
